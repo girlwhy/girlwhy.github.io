cf853a2bcc9494252f8282253235f43beaaa8c6b
girlwhy <118033152+girlwhy@users.noreply.github.com>
2022-11-11 22:03:39 -0500

    test1

--- myScript.js
+++ myScript.js
@@ -1,14 +1,17 @@
 function yesAlias{
+  alert("idk")
   var yes = document.getElementById("yesAlias");
   var aliasInput = document.getElemendById("inputAlias");
 
   if (yes.checked == true){
     aliasInput.style.display = "block";
+    alert("fuck")
   } else {
     aliasInput.style.display = "none";
   }
 }
+
 function plusAlias{
-  var node = document.getElementById("alias");
+  var node = document.getElementById("inputAlias");
   cloneNode(node);
 }
